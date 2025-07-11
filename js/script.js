964a06c0e261859a5c3b3a5eeae570f5b0d0e33d
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-11 14:55:18 +0200

    [add] variables, function to pick 5 randomly generated numbers and insert them in HTML, for cycle, comments

--- js/script.js
+++ js/script.js
@@ -10,3 +10,22 @@ const inputGroup = document.getElementById("input-group");
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// Dichiaro una variabile per salvare i numeri della funzione che verrà eseguita
+let generatedNumbers = [];
+
+// Creo un'altra funzione per prendere 5 numeri generati e inserirli direttamente in HTML
+function generateNumbers() {
+  numbersList.innerHTML = ""; // Setto il valore di numbersList vuoto per mostrare i numeri che verranno generati in HTML successivamente
+  generatedNumbers = []; // Setto generatedNumbers vuoto 
+
+  for (let i = 0; i < 5; i++) { // Col for scorro i numeri generati, fermandomi a 5 (il numero richiesto in consegna)
+    const num = getRandomInt(1, 30); // Creo la variabile num alla quale assegno il risultato della funzione getRandomInt successivamente all'essermi fermato a 5 di quei numeri
+    generatedNumbers.push(num); // Pusho num in generatedNumbers per riutilizzo
+    const li = document.createElement("li"); // Creo l'elemento li
+    li.textContent = num; // Assegno il valore di num all'elemento li creato
+    numbersList.appendChild(li); // Appendo li a numbersList (che ha ul)
+  }
+}
+
+generateNumbers(); // Invoco la funzione definita che mostra immediatamente, all'apertura della pagina i 5 numeri random
